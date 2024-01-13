2c182d1b2e7f28db33b65e36c5b1165859837f44
ArunKumarMURAPAKA <arunmurapaka@gmail.com>
2024-01-13 18:03:55 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a88997dbf6msh12f1461e628bf55p14d23djsn15f10bb6b1d5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3249144f41c80cd561eff5c7e6bf70ff";
